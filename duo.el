dae013880c2d28cacfae63f7f2c8dbfc0f1e9f6a
chimay <orduval@gmail.com>
2019-03-25 17:32:27 +0100

    version 1.2

--- duo.el
+++ duo.el
@@ -4,7 +4,7 @@
 
 ;; Author : Chimay
 ;; Name: Duo
-;; Package-Version: 1.1
+;; Package-Version: 1.2
 ;; Package-requires: ((emacs "26"))
 ;; Keywords: lisp, extensions, list, in-place, operation
 ;; URL: https://github.com/chimay/duo
